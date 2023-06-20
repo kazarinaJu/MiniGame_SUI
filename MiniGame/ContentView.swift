b7c3f6d649ae85652365c1a40cf9f0b24d791776
Julia Kazarina <120021628+kazarinaJu@users.noreply.github.com>
2023-06-20 17:01:04 +0500

    make UI without slider

--- MiniGame/ContentView.swift
+++ MiniGame/ContentView.swift
@@ -8,15 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var targetValue = Int.random(in: 0...100)
+    @State private var currentValue = 0.0
+    
+    @State private var isPresented = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(spacing: 30) {
+            Text("Подвиньте слайдер, как можно ближе к: \(targetValue)")
+            HStack {
+                Text("0")
+                //slider
+                Text("100")
+            }
+            
+            Button("Проверь меня!") { isPresented.toggle() }
+                .alert("Ваш счёт", isPresented: $isPresented, actions: {}) {
+                    Text(computeScore().formatted())
+                }
+            
+            Button("Начать заново") {
+                targetValue = Int.random(in: 0...100)
+                currentValue = 0.0
+            }
         }
         .padding()
     }
+    
+    private func computeScore() -> Int {
+            let difference = abs(targetValue - lround(Double(currentValue)))
+            return 100 - difference
+        }
 }
 
 struct ContentView_Previews: PreviewProvider {
