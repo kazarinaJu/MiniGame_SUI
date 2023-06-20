3fea0f307521bb12f8b83a08639252a09687dfc5
Julia Kazarina <120021628+kazarinaJu@users.noreply.github.com>
2023-06-20 16:42:22 +0500

    Initial Commit

---
+++ MiniGame/MiniGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniGameApp.swift
+//  MiniGame
+//
+//  Created by Юлия Ястребова on 20.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct MiniGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
